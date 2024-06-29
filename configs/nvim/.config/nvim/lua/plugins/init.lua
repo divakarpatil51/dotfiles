d06439d44782a9ac55e8696c70d6c2e01336b36c
Divakar Patil <divakarpatil35@gmail.com>
2024-06-30 04:09:23 +1000

    tidy up noice plugin

--- configs/nvim/.config/nvim/lua/plugins/init.lua
+++ configs/nvim/.config/nvim/lua/plugins/init.lua
@@ -39,34 +39,6 @@ return {
     end
   },
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-    -- config = function()
-    --   require("lualine").setup({
-    --     sections = {
-    --       lualine_x = {
-    --         {
-    --           require("noice").api.statusline.mode.get,
-    --           cond = require("noice").api.statusline.mode.has,
-    --           color = { fg = "#ff9e64" },
-    --         }
-    --       },
-    --     },
-    --   })
-    -- end
-  },
-  {
     'folke/neodev.nvim',
     opts = {
       library = {
--- configs/nvim/.config/nvim/lua/plugins/noice.lua
+++ configs/nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,47 +1,63 @@
 return {
--- 
---   "folke/noice.nvim",
---   event = "VeryLazy",
---   dependencies = {
---     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
---     "MunifTanjim/nui.nvim",
---     -- OPTIONAL:
---     --   `nvim-notify` is only needed, if you want to use the notification view.
---     --   If not available, we use `mini` as the fallback
---     "rcarriga/nvim-notify",
---   },
---   config = function()
---     require("noice").setup({
---       routes = {
---         {
---           filter = {
---             event = "msg_show",
---             kind = "",
---             find = "written",
---           },
---           opts = { skip = true },
---         },
---       },
---       lsp = {
---         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
---         override = {
---           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
---           ["vim.lsp.util.stylize_markdown"] = true,
---           ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
---         },
---       },
---       views = {
---         cmdline_popup = {
---           position = {
---             row = 25,
---             col = "50%",
---           },
---           size = {
---             width = 60,
---             height = "auto",
---           },
---         },
---       },
---     })
---   end
+
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
+  config = function()
+    require("lualine").setup({
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            require("noice").api.status.search.get,
+            cond = require("noice").api.status.search.has,
+            color = { fg = "#ff9e64" },
+          }
+        },
+      },
+    })
+    require("noice").setup({
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 25,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+        },
+      },
+    })
+  end
 }
